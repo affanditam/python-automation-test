36dfebb8afc0265e35c2c1d479142a55a7e21c15
Affandi Pratama <afanditama@yahoo.com>
2023-01-17 11:15:24 +0700

    commit pertama

---
+++ tests/integration/test_integration.py
@@ -0,0 +1,34 @@
+import unittest
+
+# test basic
+class TestBasic(unittest.TestCase):
+    def setUp(self):
+        # Load test data
+        # self.app = App(database='fixtures/test_basic.json')
+        pass
+
+    def test_customer_count(self):
+        self.assertEqual(len(self.app.customers), 100)
+
+    def test_existence_of_customer(self):
+        customer = self.app.get_customer(id=10)
+        self.assertEqual(customer.name, "Org XYZ")
+        self.assertEqual(customer.address, "10 Red Road, Reading")
+
+# test complex
+class TestComplexData(unittest.TestCase):
+    def setUp(self):
+        # load test data
+        # self.app = App(database='fixtures/test_complex.json')
+        pass
+
+    def test_customer_count(self):
+        self.assertEqual(len(self.app.customers), 10000)
+
+    def test_existence_of_customer(self):
+        customer = self.app.get_customer(id=9999)
+        self.assertEqual(customer.name, u"バナナ")
+        self.assertEqual(customer.address, "10 Red Road, Akihabara, Tokyo")
+
+if __name__ == '__main__':
+    unittest.main()
